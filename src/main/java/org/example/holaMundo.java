a39ea5c6b67c36963998ae9026927460671dff69
Sanchez@PF4N4LMS <hellveryuk@gmail.com>
2026-08-04 15:35:19 -0600

    prueba de commit rama nueva

--- src/main/java/org/example/holaMundo.java
+++ src/main/java/org/example/holaMundo.java
@@ -3,11 +3,11 @@ package org.example;
 public class holaMundo {
 
     public static void main (String[] args) {
-        System.out.println("Hola Mundo mi nombre es Javier sanchez garcia");
-        System.out.println(operacionesMatematicas.operacionSuma(3,5));
-        System.out.println(operacionesMatematicas.operacionResta(3,5));
-        System.out.println(operacionesMatematicas.operacionMultiplicacion(3,5));
-        System.out.println(operacionesMatematicas.operacionDivicion(30,5));
+        System.out.println("Hola Mundo mi nombre es Javier");
+        System.out.println(operacionesMatematicas.operacionSuma(40,550));
+        System.out.println(operacionesMatematicas.operacionResta(3,4));
+        System.out.println(operacionesMatematicas.operacionMultiplicacion(30,5));
+        System.out.println(operacionesMatematicas.operacionDivicion(300,5));
 
         //Este es un comentario
         /* este es un bloqueo de comentarios
