c0bc057fe88e218f4bcb995195ed2f3362e4705b
Sanchez@PF4N4LMS <hellveryuk@gmail.com>
2026-08-04 14:20:02 -0600

    Se creo la clase operacionesMatematicas con la funcion operacionSuma

--- src/main/java/org/example/holaMundo.java
+++ src/main/java/org/example/holaMundo.java
@@ -4,6 +4,7 @@ public class holaMundo {
 
     public static void main (String[] args) {
         System.out.println("Hola Mundo mi nombre es Javier sanchez garcia");
+        System.out.println(operacionesMatematicas.operacionSuma(3,5));
         //Este es un comentario
         /* este es un bloqueo de comentarios
 
---
+++ src/main/java/org/example/operacionesMatematicas.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class operacionesMatematicas
+{
+    public static String operacionSuma(int num1, int num2) {
+        int resultadoDeSuma=num1+num2;
+        return  "El resultado de la suma de dos numeros es: "+(num1+num2); //cadena + el valor de la suma
+    }
+
+    }
+
